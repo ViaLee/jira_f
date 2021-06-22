b3883ecec47d9118401910eb0f737e53854f5a37
Via <via.li@kreakin.com>
2021-06-22 15:09:34 +0800

    1

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,10 +1,10 @@
 import React, { FormEventHandler, FormEvent } from "react";
 
 export const LoginScreen = () => {
-  const apiUrl = process.env.REACT_APP_API_RUL;
+  const apiUrl = process.env.REACT_APP_API_URL;
 
   const login = (param: { username: string; password: string }) => {
-    fetch(`${apiUrl}/login`, {
+    fetch(`${apiUrl}/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -32,7 +32,7 @@ export const LoginScreen = () => {
         <label htmlFor="password">密码</label>
         <input type="password" id="password" />
       </div>
-      <button type="submit">登录</button>
+      <button type="submit">注册</button>
       <div className="triangle"></div>
     </form>
   );
