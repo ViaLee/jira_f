4edc2652d9d5a6e13ed2c62497d8b29023b49288
Via <via.li@kreakin.com>
2021-06-23 16:05:04 +0800

    antd

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React from "react";
 import { User } from "screens/project-list/search-panel";
-
+import { Table } from "antd";
 interface Project {
   id: string;
   name: string;
@@ -13,21 +13,30 @@ interface ListProps {
   list: Project[];
   users: User[];
 }
-export const List = ({list, users}:ListProps)=>{
-    return <table>
-    <thead>
-    <tr>
-      <th>名称</th>
-      <th>负责人</th>
-    </tr>
-    </thead>
-    <tbody>
-    {
-      list.map(project => <tr key={project.id}>
-        <td>{project.name}</td>
-        <td>{users.find(user => user.id === project.personId)?.name || '未知'}</td>
-      </tr>)
-    }
-    </tbody>
-  </table>
-}
+export const List = ({ list, users }: ListProps) => {
+  return (
+    <Table
+      pagination={false}
+      columns={[
+        {
+          title: "名称",
+          dataIndex: "name",
+          sorter: (a, b) => a.name.localeCompare(b.name),
+          //string.localeCompare(param) 0 : 字符串匹配100%, 1 : 不匹配，参数值来自于语言环境的排序顺序字符串对象的值之前,-1 : 不匹配，参数值来自于语言环境的排序顺序字符串对象的值之后
+        },
+        {
+          title: "负责人",
+          render(value, project) {
+            return (
+              <span>
+                {users.find((user) => user.id === project.personId)?.name ||
+                  "未知"}
+              </span>
+            );
+          },
+        },
+      ]}
+      dataSource={list}
+    />
+  );
+};
