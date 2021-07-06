c71fde61add42842b01950b2cfa88053926b314b
Via <via.li@kreakin.com>
2021-07-06 20:20:53 +0800

    路由

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { User } from "screens/project-list/search-panel";
 import { Table } from "antd";
+import { Link } from "react-router-dom";
 export interface Project {
   id: string;
   name: string;
@@ -25,6 +26,9 @@ export const List = ({ list, users, loading }: ListProps) => {
           title: "名称",
           dataIndex: "name",
           sorter: (a, b) => a.name.localeCompare(b.name),
+          render(v, project) {
+            return <Link to={String(project.id)}>{project.name}</Link>;
+          },
           //string.localeCompare(param) 0 : 字符串匹配100%, 1 : 不匹配，参数值来自于语言环境的排序顺序字符串对象的值之前,-1 : 不匹配，参数值来自于语言环境的排序顺序字符串对象的值之后
         },
         {
